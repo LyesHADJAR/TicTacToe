86461b9d4bc0deaded1b98e70774014227d6a68d
LyesHADJAR <lyes.hadjar@ensia.edu.dz>
2024-08-25 17:30:19 +0100

    Added the statics and templates

--- app.py
+++ app.py
@@ -2,3 +2,22 @@ from flask import Flask, request, jsonify, templating
 from minimax import MiniMaxAgent
 from tictactoe import TicTacToe
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return templating.render_template('index.html')
+
+@app.route('/play', methods=['POST'])
+def play():
+    data = request.get_json()
+    h_symbol = data['h_symbol']
+    c_symbol = data['c_symbol']
+    depth = data['depth']
+    size = data['size']
+    board = data['board']
+    game = TicTacToe(h_symbol, c_symbol, depth, size)
+    game.board = board
+    agent = MiniMaxAgent(game)
+    agent.play()
+    return jsonify(game.board)
