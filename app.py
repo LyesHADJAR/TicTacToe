440aa373e3101e309712cb229090c1b2e8ea378b
peppa234 <aichabrihmouche@gmail.com>
2024-08-26 22:00:58 +0000

    made some changes

--- app.py
+++ app.py
@@ -1,43 +1,33 @@
-from flask import Flask, request, jsonify, templating
+from flask import Flask, request, jsonify, render_template
 from minimax import MiniMaxAgent
 from tictactoe import TicTacToe
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route('/')              # This is the home page
-                             # We need to create where the user is welcomed
-                             # and can choose between the PvP mode or PvE mode
+@app.route('/')              
 def index():
-    return templating.render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/pvp', ["POST"]) # pvp for player vs player
-                             # Here we just load the game that you have creted 
+@app.route('/pvp', methods=["GET"])  # Changed to GET since wer're not making a post request 
+
 def pvp():
-    return templating.render_template('pvp.html')
+    return render_template('pvp.html')
 
-@app.route('/pve', methods=["POST"]) # pve for player vs environment
-                                     # Here I created the game and the computer will play first
-                                     # first we cann the function pve that will create the game
-                                     # after getting his choices we need to start the game
-                                     # we need AJAX to avoid the page reload after each move
+@app.route('/pve', methods=["POST"])
 def pve():
-    # Set up the game
     data = request.get_json()
     h_symbol = data['h_symbol']
     c_symbol = data['c_symbol']
     first_move = data['first_move']
     game = TicTacToe(h_symbol, c_symbol, 9, 9)
-    
-    # decide who plays first based on the user's choice
-    # we need to recieve the first move
+
     if first_move == 'computer':
         agent = MiniMaxAgent(game)
         agent.play()
-    return jsonify(game.board)
 
-@app.route('/pve/play', methods=['POST']) # Here we need to play the game
-                                          # we need the frontend for this + AJAX to avoid loading the page 
-                                          # after each move
+    return jsonify({'board': game.board, 'game_over': game.game_over(game.board), 'winner': None})
+
+@app.route('/pve/play', methods=['POST'])
 def play():
     data = request.get_json()
     board = data['board']
@@ -51,7 +41,12 @@ def play():
     if not game.game_over(game.board):
         agent.play()
         return jsonify({
-                        'board': game.board,
-                        'game_over': game.game_over(game.board),
-                        'winner': game.goal_test(game.board, game.comp) or game.goal_test(game.board, game.human)})
+            'board': game.board,
+            'game_over': game.game_over(game.board),
+            'winner': game.goal_test(game.board, game.comp) or game.goal_test(game.board, game.human)
+        })
+    else:
+        return jsonify({'board': game.board, 'game_over': True, 'winner': None})
 
+if __name__ == '__main__':
+    app.run(debug=True)
